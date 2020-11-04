4dbd1df656eb68f08286f472916f134383d9b761
Arno Paris <arnaud.courrot@gmail.com>
2020-11-04 21:55:34 +0100

    test first commit

--- src/components/Tasks/NoTasks.vue
+++ src/components/Tasks/NoTasks.vue
@@ -1,27 +1,19 @@
 <template>
-  <transition
-    appear
-    enter-active-class="animated zoomIn"
-    leave-active-class="animated zoomOut absolute-top"
-  >
-  	<q-banner class="bg-grey-3">
-      <template v-slot:avatar>
-        <q-icon name="check" color="primary" />
-      </template>
-      No tasks to do today!
-      <template v-slot:action>
-        <q-btn
-          @click="$root.$emit('showAddTask')"
-        	flat
-        	color="primary"
-        	label="Add Task" />
-      </template>
+<transition appear enter-active-class="animated zoomIn" leave-active-class="animated zoomOut absolute-top">
+    <q-banner class="bg-grey-3">
+        <template v-slot:avatar>
+            <q-icon name="check" color="primary" />
+        </template>
+        Pas de task tasks aujourd'hui
+        <template v-slot:action>
+            <q-btn @click="$root.$emit('showAddTask')" flat color="primary" label="Add Task" />
+        </template>
     </q-banner>
-  </transition>
+</transition>
 </template>
 
 <script>
-	export default {
-	
-	}
+export default {
+
+}
 </script>
